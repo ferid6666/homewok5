a03342dde98352b6a6827c6eab8c13e61e522adb
ferid6666 <faridaha@code.edu.az>
2022-10-09 14:22:39 +0400

    task added

---
+++ stringtasks/Program.cs
@@ -0,0 +1,131 @@
+﻿//using System;    
+//    public class StringExample    
+//    {    
+//        public static void Main(string[] args)    
+//        {    
+//             string s1 = "Hello ";    
+//             string s2 = "He";  
+//             string s3 = "Hi";  
+//             Console.WriteLine(s1.Contains(s2));  
+//             Console.WriteLine(s1.Contains(s3));  
+
+//        }    
+//    }    
+
+
+
+
+//using System;
+//public class StringExample
+//{
+//    public static void Main(string[] args)
+//    {
+//        string s1 = "Hello";
+//        string s2 = "Hello";
+//        string s3 = "Bye";
+//        Console.WriteLine(s1.Equals(s2));
+//        Console.WriteLine(s1.Equals(s3));
+//    }
+//}
+
+
+//using System;
+//public class StringExample
+//{
+//    public static void Main(string[] args)
+//    {
+//        string s2 = "Hello p414";
+//        CharEnumerator ch = s2.GetEnumerator();
+//        while (ch.MoveNext())
+//        {
+//            Console.WriteLine(ch.Current);
+//        }
+//    }
+//}
+
+//using System;
+
+//public class StringExample
+//{
+//    public static void Main(string[] args)
+//    {
+//        string s1 = "Hello p414";
+//        string s3 = s1.ToUpper();
+//        Console.WriteLine(s3);
+//    }
+//}
+
+
+//using System;
+
+//public class StringExample
+//{
+//    public static void Main(string[] args)
+//    {
+//        string s1 = "Hello p414";
+//        string s2 = s1.ToLowerInvariant();
+//        Console.WriteLine(s2);
+//    }
+
+//}
+
+
+//using System;
+//public class StringExample
+//{
+//    public static void Main(string[] args)
+//    {
+//        string s1 = "Hello";
+//        string s2 = "llo";
+//        string s3 = "p414";
+//        Console.WriteLine(s1.EndsWith(s2));
+//        Console.WriteLine(s1.EndsWith(s3));
+//    }
+//}
+
+
+
+//using System;
+
+//public class StringExample
+//{
+//    public static void Main(string[] args)
+//    {
+//        string s1 = "Hello p414";
+//        string s2 = s1.Trim();
+//        Console.WriteLine(s2);
+//    }
+//}
+
+
+
+
+//using System;
+
+//public class StringExample
+//{
+//    public static void Main(string[] args)
+//    {
+//        string s1 = "Hello p414";
+//        int a = 666;
+//        string s2 = s1.ToString();
+//        string s3 = a.ToString();
+//        Console.WriteLine(s2);
+//        Console.WriteLine(s3);
+//    }
+//}
+
+
+
+
+//using System;
+
+//public class StringExample
+//{
+//    public static void Main(string[] args)
+//    {
+//        string s1 = "Hello p414";
+//        string s2 = s1.ToLower();
+//        Console.WriteLine(s2);
+//    }
+//}
